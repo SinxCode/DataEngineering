e04eb88139e6dae2ed44c18eb3f86e5ef39e9455
Sinx <gabriel.jbrito2@gmail.com>
2025-03-25 09:44:59 -0300

    Fusão de 2 empresas

---
+++ Scripts/fusao_empresas.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+#Leitura dos dados
+dados_json = pd.read_json('C:/Users/gabrielbrito/Alura/PythonParaDataScience/Data_Raw/dados_empresaA.json')
+dados_csv = pd.read_csv('C:/Users/gabrielbrito/Alura/PythonParaDataScience/Data_Raw/dados_empresaB.csv')
+
+#Tratamento dos dados
+dados_csv.columns=['Nome do Produto', 'Categoria do Produto', 'Preço do Produto (R$)', 'Quantidade em Estoque', 'Filial', 'Data da Venda']
+
+#Unindo os dados
+dados_combinados = pd.concat([dados_json, dados_csv], ignore_index=True)
+
+#Salvando os dados
+dados_combinados.to_csv('C:/Users/gabrielbrito/Alura/PythonParaDataScience/Data_Processed/dados_combinados.csv',index=False, encoding='utf-8')
